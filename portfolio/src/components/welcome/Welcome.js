cdf62dd33d3ec1a60150df9953c77871e705a498
dbarrios13 <barriosadavid@gmail.com>
2019-07-02 22:49:11 -0700

    fix bugs for mobile version

--- portfolio/src/components/welcome/Welcome.js
+++ portfolio/src/components/welcome/Welcome.js
@@ -15,57 +15,96 @@ import {
 export default class Welcome extends React.Component {
 
     componentDidMount() {
+        const lines = document.querySelectorAll('.line')
+        const subtitles = Array.from(document.querySelectorAll('.subtitle'))
+        const letters = document.querySelectorAll('.letters')
+        let elements = []
+
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            elements.push(lines)
+            subtitles.map(e => {
+               return e.classList.add('active')
+            })
+        } else {
+            subtitles.map(e => {
+                if (e.classList.contains('active')) {
+                    return e.classList.remove('active')
+                } else {
+                    return e;
+                }
+            })
+            elements.push(lines)
+            elements.push(letters)
+        }
+
         const logoAnimation = anime.timeline({
             direction: 'alternate',
             loop: false
         })
 
-        const lines = document.querySelectorAll('.line')
-
-        logoAnimation.add(
-            {
-                targets: [lines, '.letters'],
-                translateX: target => {
-                    let x = 2200
-                    let translate;
-                    if (target.classList.contains('hori')) {
-                        translate = anime.random(0, 1) ? x : -x
-                    }
-                    if (target.classList.contains('diag-right') || target.classList.contains('diag-left')) {
-                        translate = x / 3
-                    }
-                    return [translate, 0]
-
-                },
-                translateY: target => {
-                    let y = 2200
-                    let translate
-                    if (target.classList.contains('vert')) {
-                        translate = anime.random(0, 1) ? y : -y
-                    }
-                    if (target.classList.contains('diag-right')) {
-                        translate = -y / 3
-                    }
-                    if (target.classList.contains('diag-left')) {
-                        translate = y / 3
-                    }
-                    return [translate, 0]
-                },
-                scale: {
-                    value: [6, 1],
-                    duration: 500 * 2
-                },
-                stroke: '#66fcf1',
-                opacity: {
-                    value: [0, 1],
-                    duration: 100 * 4
-                },
-                delay: (t, i) => (i * 20),
-                duration: 500,
-                easing: 'easeOutQuart',
-                offset: 0
-            }
-        )
+        logoAnimation.add({
+            targets: elements,
+            translateX: target => {
+                let x = 1200
+                let translate;
+                if (target.classList.contains('hori')) {
+                    translate = anime.random(0, 1) ? x : -x
+                }
+                if (target.classList.contains('diag-right') || target.classList.contains('diag-left')) {
+                    translate = x / 3
+                }
+                return [translate, 0]
+            },
+            translateY: target => {
+                let y = 1200
+                let translate
+                if (target.classList.contains('vert')) {
+                    translate = anime.random(0, 1) ? y : -y
+                }
+                if (target.classList.contains('diag-right')) {
+                    translate = -y / 3
+                }
+                if (target.classList.contains('diag-left')) {
+                    translate = y / 3
+                }
+                return [translate, 0]
+            },
+            scale: {
+                value: [4, 1],
+                duration: 500
+            },
+            stroke: '#66fcf1',
+            opacity: {
+                value: [0, 1],
+                duration: 100
+            },
+            delay: (t, i) => (i * 25),
+            duration: 500,
+            easing: 'easeOutQuart',
+            offset: 0
+        })
+        
+        .add({
+            targets: '.active text',
+            translateX: () => {
+                let translate = 1200
+                // let translate = anime.random(0, 1200)
+                return [0, translate]
+            },
+            translateY: () => {
+                // let translate = anime.random(-1200, 1200)
+                let translate = 1200
+                return [0, translate]
+            },
+            opacity: {
+                value: [0, 1],
+                duration: 100
+            },
+            delay: (t, i) => (i * 50),
+            duration: 500 * 2,
+            easing: 'easeOutQuart',
+            offset: 0
+        })
     }
 
     toProjects = () => {
@@ -81,7 +120,7 @@ export default class Welcome extends React.Component {
             {
                 targets: [lines, letters],
                 translateX: target => {
-                    let x = 2200
+                    let x = 1200
                     let translate;
                     if (target.classList.contains('hori')) {
                         translate = anime.random(0, 1) ? x : -x
@@ -93,7 +132,7 @@ export default class Welcome extends React.Component {
 
                 },
                 translateY: target => {
-                    let y = 2200
+                    let y = 1200
                     let translate
                     if (target.classList.contains('vert')) {
                         translate = anime.random(0, 1) ? y : -y
@@ -107,7 +146,7 @@ export default class Welcome extends React.Component {
                     return [0, translate]
                 },
                 scale: {
-                    value: [1, 6],
+                    value: [1, 4],
                     duration: 500 * 2
                 },
                 stroke: '#66fcf1',
@@ -131,11 +170,11 @@ export default class Welcome extends React.Component {
             <div className='welcome' >
                 <svg className='logo' height='100%' width='100%' viewBox='0 0 1024 448'>
                     <g stroke='#66fcf1' strokeWidth='2' fill='none' fillRule='evenodd'>
-                        <Dpath x={180} y={100} />
-                        <Apath x={230} y={100} />
-                        <Vpath x={270} y={100} />
-                        <Ipath x={335} y={100} />
-                        <Dpath x={360} y={100} />
+                        <Dpath x={200} y={100} />
+                        <Apath x={255} y={100} />
+                        <Vpath x={300} y={100} />
+                        <Ipath x={365} y={100} />
+                        <Dpath x={390} y={100} />
                         <Bpath x={480} y={100} />
                         <Apath x={535} y={100} />
                         <Rpath x={595} y={100} />
@@ -143,8 +182,8 @@ export default class Welcome extends React.Component {
                         <Ipath x={715} y={100} />
                         <Opath x={740} y={100} />
                         <Spath x={800} y={100} />
-                        </g>
-                        <g stroke='#66fcf1' strokeWidth='2' fill='none' fillRule="evenodd">
+                    </g>
+                    <g className='subtitle' stroke='#66fcf1' strokeWidth='2' fill='none' fillRule="evenodd">
                         <text x='244' y='275' className='letters hori' >S</text>
                         <text x='279' y='275' className='letters vert' >o</text>
                         <text x='314' y='275' className='letters diag-right' >f</text>
@@ -161,17 +200,17 @@ export default class Welcome extends React.Component {
                         <text x='679' y='275' className='letters hori' >e</text>
                         <text x='714' y='275' className='letters diag-right' >e</text>
                         <text x='749' y='275' className='letters diag-left' >r</text>
-                        </g>
-                        <g>
+                    </g>
+                    <g className='subtitle' stroke='#66fcf1' strokeWidth='1' fill='none' fillRule='evenodd'>
                         <a href={this.toProjects} onClick={this.toProjects}>
-                            <text x='450' y='350' strokeWidth='1' className='letters hori link'>P</text>
-                            <text x='470' y='350' strokeWidth='1' className='letters vert link'>r</text>
-                            <text x='483' y='350' strokeWidth='1' className='letters diag-left link'>o</text>
-                            <text x='500' y='350' strokeWidth='1' className='letters diag-right link'>j</text>
-                            <text x='508' y='350' strokeWidth='1' className='letters vert link'>e</text>
-                            <text x='525' y='350' strokeWidth='1' className='letters diag-right link'>c</text>
-                            <text x='542' y='350' strokeWidth='1' className='letters diag-left link'>t</text>
-                            <text x='555' y='350' strokeWidth='1' className='letters hori link'>s</text>
+                            <text x='450' y='350' className='letters hori link'>P</text>
+                            <text x='470' y='350' className='letters vert link'>r</text>
+                            <text x='483' y='350' className='letters diag-left link'>o</text>
+                            <text x='500' y='350' className='letters diag-right link'>j</text>
+                            <text x='508' y='350' className='letters vert link'>e</text>
+                            <text x='525' y='350' className='letters diag-right link'>c</text>
+                            <text x='542' y='350' className='letters diag-left link'>t</text>
+                            <text x='555' y='350' className='letters hori link'>s</text>
                         </a>
                     </g>
                 </svg>
