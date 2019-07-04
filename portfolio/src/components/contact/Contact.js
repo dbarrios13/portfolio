43cc6502c5374e1b032a1b02da17e39a25078648
dbarrios13 <barriosadavid@gmail.com>
2019-07-04 15:26:31 -0700

    added responsive styles for about and contact sections

--- portfolio/src/components/contact/Contact.js
+++ portfolio/src/components/contact/Contact.js
@@ -10,8 +10,8 @@ export default class Contact extends React.Component {
 
     render() {
         return (
-            <div id='contact' className='contact' data-aos='fade-up' data-aos-duration>
-                <div className='form'>
+            <div id='contact' className='contact'>
+                <div className='form' data-aos='fade-up' data-aos-duration>
                     <form name='contact' method='POST' data-netlify='true'>
                         <input name='name' type='text' placeholder='Name' />
                         <input name='email' type='email' placeholder='Email' />
@@ -19,11 +19,11 @@ export default class Contact extends React.Component {
                         <button type='submit'>Send Message</button>
                     </form>
                 </div>
-                <div className='form-message'>
+                <div className='form-message' data-aos='fade-up' data-aos-duration>
                     <h3>Contact</h3>
                     <p>Feel free to send me a message about any questions you may have or just to get in contact with me. Thank you for visiting my portfolio and I look forward to speaking with you soon.</p>
                 </div>
-                <div className='btn'>
+                <div className='btn' data-aos='fade-up' data-aos-duration>
                     <button onClick={this.scrollTop} className='btn-top'>Back to Top</button>
                 </div>
             </div>
