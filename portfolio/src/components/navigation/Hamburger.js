6dc49ef1d4d63eb14c87e05f3165d050f8355d4b
David Barrios <David Barrios>
2019-09-04 16:24:44 -0700

    fixed hamburger menu to close on click on links

--- portfolio/src/components/navigation/Hamburger.js
+++ portfolio/src/components/navigation/Hamburger.js
@@ -2,29 +2,11 @@ import React from 'react';
 import Button from './hamburgerComponents/Button';
 import Menu from './hamburgerComponents/Menu';
 
-export default class Hamburger extends React.Component {
-    state = {
-        menuOpen: false
-    }
-
-    hamburgerToggle = () => {
-        this.setState({
-            menuOpen: !this.state.menuOpen
-        })
-    }
-
-    linkClick = () => {
-        this.setState({
-            menuOpen: false
-        })
-    }
-
-    render () {
-        return(
-            <>
-                <Button />
-                <Menu close={this.linkClick}/>
-            </>
-        )
-    }
+export default function Hamburger() {
+    return (
+        <>
+            <Button />
+            <Menu />
+        </>
+    )
 }
--- portfolio/src/components/navigation/hamburgerComponents/Menu.js
+++ portfolio/src/components/navigation/hamburgerComponents/Menu.js
@@ -1,27 +1,32 @@
 import React from 'react'
 import smoothscroll from 'smoothscroll-polyfill'
 
-function scroll(e, props) {
-    e.preventDefault()
-    smoothscroll.polyfill()
-    document.querySelector(e.target.hash).scrollIntoView({ behavior: 'smooth' })
-    props.close()
-}
-
-export default function Menu() {
-    return (
-        <div className='hamburgerMenu'>
-            <ul>
-                <li>
-                    <a href='#projects' onClick={scroll}>Projects</a>
-                </li>
-                <li>
-                    <a href='#about' onClick={scroll}>About</a>
-                </li>
-                <li>
-                    <a href='#contact' onClick={scroll}>Contact</a>
-                </li>
-            </ul>
-        </div>
-    )
+export default class Menu extends React.Component {
+    scroll = e => {
+        e.preventDefault()
+        smoothscroll.polyfill()
+        document.querySelector(e.target.hash).scrollIntoView({ behavior: 'smooth' })
+        const menu = document.querySelector('.hamburgerMenu')
+        const span = document.querySelector('.hamburgerBtn span')
+        span.classList.remove('hamburgerAni')
+        menu.classList.remove('hamburgerMenuAni')
+    }
+    
+    render() {
+        return (
+            <div className='hamburgerMenu'>
+                <ul>
+                    <li>
+                        <a href='#projects' onClick={this.scroll}>Projects</a>
+                    </li>
+                    <li>
+                        <a href='#about' onClick={this.scroll}>About</a>
+                    </li>
+                    <li>
+                        <a href='#contact' onClick={this.scroll}>Contact</a>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
 }
